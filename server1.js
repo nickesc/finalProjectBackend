3ec048f2b1ba6c5da3efe42376dcdddea2c4dfff
Nick <nickesc.gd@gmail.com>
2021-05-08 14:46:52 -0400

    1

--- server1.js
+++ server1.js
@@ -2,6 +2,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const fs = require ("fs-extra");
 const cors=require("cors");
+const fetch = require("node-fetch");
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://nick:portPass@cluster0.onn1x.mongodb.net/portfolio?retryWrites=true&w=majority";
@@ -116,14 +117,143 @@ async function postComment(req, res){
 
 }
 async function getMech(req, res){
-	projection={title:1, url:1, help:1}
+	//projection={title:1, url:1, help:1}
 
 	let query={help:"1"}
-	let projectsCursor = await collection1.find(query).project(projection);
+	let projectsCursor = await collection1.find();
 	let digital = await projectsCursor.toArray();
 
 	res.json(digital);
 
+}
+
+async function updateList(out, collection){
+for (i=0; i<out.data.children.length; i++){
+			
+			var obj = out.data.children[i].data
+
+			obj._id=obj.name;
+
+			delete obj.approved_at_utc;
+			delete obj.saved;
+			delete obj.mod_reason_title;
+			delete obj.gilded;
+			delete obj.clicked;
+			delete obj.link_flair_richtext;
+			delete obj.hidden;
+			delete obj.pwls;
+			delete obj.top_awarded_type;
+			delete obj.hide_score;
+			delete obj.quarantine;
+			delete obj.upvote_ratio
+			delete obj.author_flair_background_color
+			delete obj.subreddit_type
+			delete obj.media_embed
+			delete obj.author_flair_template_id
+			delete obj.is_original_content
+			delete obj.user_reports
+			delete obj.secure_media
+			delete obj.is_reddit_media_domain
+			delete obj.is_meta
+			delete obj.category
+			delete obj.secure_media_embed
+			delete obj.can_mod_post
+			delete obj.approved_by
+			delete obj.author_premium
+			delete obj.thumbnail
+			delete obj.edited
+			delete obj.author_flair_css_class
+			delete obj.author_flair_richtext
+			delete obj.gildings
+			delete obj.content_categories
+			delete obj.is_self
+			delete obj.mod_note
+			delete obj.link_flair_type
+			delete obj.removed_by_category
+			delete obj.banned_by
+			delete obj.author_flair_type
+			delete obj.domain
+			delete obj.allow_live_comments
+			delete obj.selftext_html
+			delete obj.suggested_sort
+			delete obj.banned_at_utc
+			delete obj.view_count
+			delete obj.archived
+			delete obj.no_follow
+			delete obj.is_crosspostable
+			delete obj.pinned
+			delete obj.over_18
+			delete obj.all_awardings
+			delete obj.awarders
+			delete obj.media_only
+			delete obj.link_flair_template_id
+			delete obj.can_gild
+			delete obj.spoiler
+			delete obj.locked
+			delete obj.author_flair_text
+			delete obj.treatment_tags
+			delete obj.visited
+			delete obj.removed_by
+			delete obj.num_reports
+			delete obj.distinguished
+			delete obj.mod_reason_by
+			delete obj.removal_reason
+			delete obj.link_flair_background_color
+			delete obj.is_robot_indexable
+			delete obj.report_reasons
+			delete obj.discussion_type
+			delete obj.send_replies
+			delete obj.whitelist_status
+			delete obj.contest_mode
+			delete obj.mod_reports
+			delete obj.author_patreon_flair
+			delete obj.author_flair_text_color
+			delete obj.permalink
+			delete obj.parent_whitelist_status
+			delete obj.stickied
+			delete obj.subreddit_subscribers
+			delete obj.num_crossposts
+			delete obj.media
+			delete obj.is_video
+			delete obj.link_flair_text_color
+
+			let query={_id:obj._id};
+			let findResult = await collection.findOne(query);
+
+			if (findResult!=null){
+				console.log("" + i + " already exists")
+			} else{
+				const result = await collection.insertOne(obj)
+				console.log("" + result.insertedId + " inserted")
+			}
+			//console.log(findResult)
+
+			
+/*
+			try {
+				let findResult = await collection.findOne(query);
+				if (findResult!=null){
+					console.log(findResult._id);
+					console.log(findResult._id);
+				}
+				
+			} catch (err) {
+				console.error(`Something went wrong: ${err}`)
+			}
+			*/
+			//let findResult= await collection.findOne(query);
+			//console.log(findResult);
+
+			//if ()
+			//	if (err) console.log("already exists");
+			//	const res = await collection.insertOne(obj)
+			//	console.log('Inserted '+res.insertedId)
+			//})
+  				
+			//const result = collection.insertOne(obj)
+
+			//console.log('Checkout this JSON! ', result)
+		}
 }
 
 app.get("/projects",getProjects);
@@ -132,14 +262,23 @@ app.get("/projects/:project",getContent);
 app.get("/featured",getFeatured);
 app.post("/comment/:project",jsonParser, postComment)
 
-var minutes = 1, the_interval = minutes * 60 * 1000;
-setInterval(function() {
+var minutes = 1, the_interval = minutes * 5 * 1000;
+setInterval(async function() {
   console.log("I am doing my 5 minutes check");
-  let url="https://www.reddit.com/r/mechmarket/search/.json?q=flair%3A%22group%20buy%22&restrict_sr=1&sort=new&limit=100";
-  fetch(url)
+  let gbUrl="https://www.reddit.com/r/mechmarket/search/.json?q=flair%3A%22group%20buy%22&restrict_sr=1&sort=new&limit=20";
+  let icUrl="https://www.reddit.com/r/mechmarket/search/.json?q=flair%3A%22interest%20check%22&restrict_sr=1&sort=new&limit=20";
+  fetch(gbUrl)
 	.then(res => res.json())
 	.then((out) => {
-  		console.log('Checkout this JSON! ', out);
+		updateList(out, collection1);
+  		
+	})
+	.catch(err => { throw err });
+  fetch(icUrl)
+	.then(res => res.json())
+	.then((out) => {
+		updateList(out, collection2);
+  		
 	})
 	.catch(err => { throw err });
   // do your stuff here
